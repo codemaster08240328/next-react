503b387ff8f2d51d5aee3b4bb2ba3a9c2aadd9f5
Pavlo Prydryk <codemaster08240328@gmail.com>
2019-02-09 02:54:28 +0000

    add vue card

--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,11 +1,27 @@
 <template lang="html">
   <div>
-    <h1> Hello From results route {{$route.params.id}} </h1>
+    <h1> Results for {{$route.params.id}} </h1>
+    <div v-if="albumExists">
+      <div v-for="(album, index) in albumData">
+        <Card
+          :artistName="album.artistName"
+          :title="album.collectionCensoredName"
+          :image="album.artworkUrl100"
+          :color="purple"
+        />
+      </div>
+
     {{albumData}}
+    </div>
+    <div v-else>
+      <h3>Could Not Find Album</h3>
+    </div>
+
   </div>
 </template>
 <script>
 import axios from "axios";
+import Card from "~/components/Card";
 
 export default {
   asyncData({ params }) {
@@ -15,6 +31,19 @@ export default {
         return { albumData: response.data.results };
       });
   },
-  middleware: "search"
+  components: {
+    Card
+  },
+  middleware: "search",
+  computed: {
+    albumExists() {
+      return this.albumData.length > 0;
+    }
+  },
+  methods: {
+    picker(index) {
+      return index % 2 == 0 ? "red" : "blue";
+    }
+  }
 };
 </script>
